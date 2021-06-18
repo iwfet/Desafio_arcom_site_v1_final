74c0550c3086f27d0791c195cb16fbae35ac882f
Joao Vitor Vieira Claro <joao.claro@arcomsa.ad>
2021-06-18 16:02:58 -0300

    subindo site heroku

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
 from loja import app
+import os
 
-if __name__ =="__main__":            
-      app.run(debug= True)
+def main():
+      port = int(os.environ.get("PORT", 5000))
+      app.run(host="0.0.0.0", port=port)
+
+if __name__ =="__main__":         
+      main()
     
     
     
